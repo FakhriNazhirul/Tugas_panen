75622d67b503345d791930f49777e05b62bb515a
unknown <fakhri.nazhirul@mhs.itenas.ac.id>
2025-12-10 22:26:22 +0700

    pengerjaan pertama

---
+++ tugas.py
@@ -0,0 +1,48 @@
+data_panen = {
+    'lokasi1': {
+        'nama_lokasi': 'Kebun A',
+        'hasil_panen': {
+            'padi': 1200,
+            'jagung': 800,
+            'kedelai': 500
+        }
+    },
+    'lokasi2': {
+        'nama_lokasi': 'Kebun B',
+        'hasil_panen': {
+            'padi': 1500,
+            'jagung': 900,
+            'kedelai': 450
+        }
+    },
+    'lokasi3': {
+        'nama_lokasi': 'Kebun C',
+        'hasil_panen': {
+            'padi': 1100,
+            'jagung': 750,
+            'kedelai': 600
+        }
+    },
+    'lokasi4': {
+        'nama_lokasi': 'Kebun D',
+        'hasil_panen': {
+            'padi': 1300,
+            'jagung': 850,
+            'kedelai': 550
+        }
+    },
+    'lokasi5': {
+        'nama_lokasi': 'Kebun E',
+        'hasil_panen': {
+            'padi': 1400,
+            'jagung': 950,
+            'kedelai': 480
+        }
+    }
+}
+# No 1
+print("--- SEMUA DATA ---")
+for info_panen in data_panen.values():
+     tempat = info_panen['nama_lokasi'] 
+     panen = info_panen['hasil_panen']
+     print(f"lokasi berikut dari{tempat}, hasil panennya adalah {panen}")
